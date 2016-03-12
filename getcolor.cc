c5a1f103191ef5fc9d4226eb38376a43b1a36a5a
jeremyqzt <jq357@hotmail.com>
2016-03-11 19:58:11 -0500

    Should make Box now, colouring is a bit off

--- getcolor.cc
+++ getcolor.cc
@@ -25,84 +25,82 @@
 #include <algorithm>
 
 using namespace std;
+double clamp(double d, double min, double max); 
 
-double clampInline(double d, double min, double max) 
-{
-  const double t = d < min ? min : d;
-  return t > max ? max : t;
-}
-
-
-void clamp(vec3 v, double min, double max) 
-{
-  v.x = clampInline(v.x,min,max);
-  v.y = clampInline(v.y,min,max);
-  v.z = clampInline(v.z,min,max);
-}
 
 void lighting(const vec3 &n, const vec3 &color, const vec3 &pos, const vec3 &direction,  vec3 &outV)
 {
 	static double CamLightW = 1.8;// 1.27536;
 	static double CamLightMin = 0.3;// 0.48193;
 	vec3 CamLight, nn;
-	VEC(CamLight, 1.0, 1.0, 1.0);
-
-	
-  VEC(nn, n.x-1.0, n.y -1.0, n.z - 1.0);
-  double dotResult = (nn.x*n.x + nn.y*n.y + nn.z*n.z);
+	VEC(CamLight, 1.0, 1.0, 1.0);	
+  VEC(nn, (n.x - 1.0), (n.y - 1.0), (n.z - 1.0));
+  double dotResult = (nn.x*direction.x) + (nn.y*direction.y + (nn.z*direction.z);
   double ambient = max( CamLightMin, dotResult )*CamLightW;
-  //  outV = CamLight*ambient*color;
-  VEC(outV, CamLight.x*ambient*color.x, CamLight.y*ambient*color.y, CamLight.z*ambient*color.z);
-  /*
-  outV.x = CamLight.x*ambient*color.x;
-  outV.y = CamLight.y*ambient*color.y;
-  outV.z = CamLight.z*ambient*color.z;*/
+  //  outV = CamLight*ambient*color; Camlight= 1 anyways...
+  VEC(outV, ambient*color.x, ambient*color.y, ambient*color.z);
 }
 
-vec3 getColour(const pixelData &pixData, const RenderParams &render_params,
-	       const vec3 &from, const vec3  &direction)
-{
+vec3 getColour(const pixelData &pixData, const RenderParams &render_params, const vec3 &from, const vec3  &direction){
 	vec3 baseColor, backColor, hitColor;
 	VEC(baseColor, 1.0, 1.0, 1.0);
-  VEC(backColor, 0.40, 0.40, 0.40);
+  VEC(backColor, 0.4, 0.4, 0.4);
   //colouring and lightning
-  hitColor = baseColor;
+  VEC(hitColor, 1.0, 1.0, 1.0);
   
-  if (pixData.escaped == false) 
-    {
+  if (pixData.escaped == false){
       //apply lighting
       lighting(pixData.normal, hitColor, pixData.hit, direction, hitColor);
-      
       //add normal based colouring
-      if(render_params.colourType == 0 || render_params.colourType == 1)
-	{
-		/*hitColor = hitColor * pixData.normal;
-	  hitColor = (hitColor + 1.0)/2.0;
-	  hitColor = hitColor*render_params.brightness;*/
-	
-	  hitColor.x = ((hitColor.x * pixData.normal.x + 1.0)/2.0)*render_params.brightness;
-	  hitColor.y = ((hitColor.y * pixData.normal.y + 1.0)/2.0)*render_params.brightness;
-	  hitColor.z = ((hitColor.z * pixData.normal.z + 1.0)/2.0)*render_params.brightness;
-	  
-	  //gamma correction
-	  clamp(hitColor, 0.0, 1.0);
-		hitColor.x *= hitColor.x;
-		hitColor.y *= hitColor.y;
-		hitColor.z *= hitColor.z;
-	}
-      if(render_params.colourType == 1)
-	{
-	  //"swap" colors
-	  double t = hitColor.x;
-	  hitColor.x = hitColor.z;
-	  hitColor.z = t;
-	}
-    }
-  else 
+      if(render_params.colourType == 0 || render_params.colourType == 1){
+				/*hitColor = hitColor * pixData.normal;
+	  		hitColor = (hitColor + 1.0)/2.0;
+	  		hitColor = hitColor*render_params.brightness;*/
+	  		hitColor.x *= pixData.normal.x;
+	  		hitColor.y *= pixData.normal.y;  			  		
+	  		hitColor.z *= pixData.normal.z;
+	  		hitColor.x = (hitColor.x+1.0)/2.0;
+	  		hitColor.y = (hitColor.y+1.0)/2.0;
+	  		hitColor.z = (hitColor.z+1.0)/2.0;
+	  		hitColor.x *= render_params.brightness;
+	  		hitColor.y *= render_params.brightness;  			  		
+	  		hitColor.z *= render_params.brightness;
+
+				//gamma correction
+				hitColor.x = clamp(hitColor.x, 0.0, 1.0);
+				hitColor.y = clamp(hitColor.y, 0.0, 1.0);
+				hitColor.z = clamp(hitColor.z, 0.0, 1.0);
+				VEC(hitColor, hitColor.x*hitColor.x, hitColor.y*hitColor.y, hitColor.z*hitColor.z);
+			}
+			double t;
+      if(render_params.colourType == 1){
+				//"swap" colors
+				t = hitColor.x;
+				hitColor.x = hitColor.z;
+				hitColor.z = t;
+			}
+ }else{
     //we have the background colour
     hitColor = backColor;
-  
+ }
   return hitColor;
 }
+
+double clamp(double d, double min, double max) {
+  double t;
+  if (d< min){
+  	t = min;
+  }else{
+  	t = d;
+  }
+  if (d > max){
+  	t = max;
+  }else{
+  	t = d;
+  }
+  
+	return t;
+}
+
 
 
